1c34796e47ab9aa7643d1e4fe12d32d595d37495
Madhup <madhupnetfundu@gmail.com>
2022-11-28 13:28:35 +0530

    1-json related work

---
+++ Python-Running-Notes/11_Working_with_Json/tabular_style_printing.py
@@ -0,0 +1,5 @@
+# print("{:<10} {:<10} {:<10}".format('NAME', 'AGE', 'COURSE'))
+# By default, `.format()` aligns text to the left, numbers to the right. 
+# You can pass an optional `<`,`^`, or `>` to set a left, center or right alignment:
+print("{:<20} {:20} {:20} {:20}".format('Step Name', 'Start Time', 'End Time', 'Elapsed Time'))
+print("-" *76)
