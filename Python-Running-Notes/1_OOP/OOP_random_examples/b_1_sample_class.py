7514f7cecefd26a61c5459345b4aa3c2c59e1c96
Madhup Srivastawa <madhup.srivastawa@oracle.com>
2023-11-07 06:21:54 +0530

    making changes to b1_sample_class.py

--- Python-Running-Notes/1_OOP/OOP_random_examples/b_1_sample_class.py
+++ Python-Running-Notes/1_OOP/OOP_random_examples/b_1_sample_class.py
@@ -4,3 +4,14 @@ class Sample():
 my_Sample = Sample()
 
 print (type(my_Sample))
+
+"""
+what this code does:
+
+It defines a class named Sample but doesn't specify any attributes or methods, so the class is empty.
+It creates an instance of the Sample class and assigns it to the variable my_Sample. This means you now have an object of the Sample class.
+It prints the type of the my_Sample object, which will be the class type that the object belongs to, in this case, Sample.
+When you run this code, it will print something like:
+<class '__main__.Sample'>
+Here, __main__ is the name of the current module (the script itself), and Sample is the class name. This output shows that the my_Sample object is of class type Sample in the __main__ module.
+"""
